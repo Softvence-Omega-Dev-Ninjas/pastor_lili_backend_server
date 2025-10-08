41d6a8ee9e1de48e7ec1e5fad1b9da2a3fd9fd2a
ranamiah221 <ranarasul21@gmail.com>
2025-10-09 01:04:01 +0600

    added verify otp for forget password

--- src/auth/dto/forgetPassword.dto.ts
+++ src/auth/dto/forgetPassword.dto.ts
@@ -2,14 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength, IsString } from 'class-validator';
 
 export class ForgetPasswordDto {
-  @ApiProperty({ example: 'john@example.com', description: 'Email of the user' })
+  @ApiProperty({ example: 'john@gmail.com', description: 'Email of the user' })
   @IsEmail()
   email: string;
-
-  @ApiProperty({ example: '1234', description: '4-digit OTP sent to email for password reset' })
-  @IsNotEmpty()
-  @IsString()
-  otp: string;
 
   @ApiProperty({ example: 'NewPassword123', description: 'New password for the account' })
   @IsNotEmpty()
