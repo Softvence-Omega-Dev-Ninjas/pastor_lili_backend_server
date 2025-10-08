aec91efb78d38cb9f3b18afb5e587b0b91c1a34a
ranamiah221 <ranarasul21@gmail.com>
2025-10-09 00:26:10 +0600

    added user profile update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { UpdateUserDto } from './dto/userUpdate.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findById(id: string) {
-    return this.prisma.user.findUnique({ where: { id } });
+    const user = await this.prisma.user.findUnique({
+      where: { id }, select: {
+        fullName: true,
+        email: true,
+        avatar: true,
+        role: true,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException("Your Account is Not Found.")
+    }
+    return user;
   }
 
-  async updateProfile(id: string, data: any) {
-    return this.prisma.user.update({ where: { id }, data });
+  async updateProfile(id: string, dto: UpdateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { id }
+    })
+    if (!user) {
+      throw new NotFoundException("Your Account is Not Found.")
+    }
+    return this.prisma.user.update({
+      where: { id }, data: { ...dto }, select: {   
+        fullName: true,
+        email: true,
+        avatar: true,
+        role: true,
+      },
+    });
   }
 
   async findByEmail(email: string) {
