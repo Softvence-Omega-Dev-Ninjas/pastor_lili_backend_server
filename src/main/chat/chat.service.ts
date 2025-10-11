0644e55996270d6cc69f759aed3ac7da33255ed2
ranamiah221 <ranarasul21@gmail.com>
2025-10-12 02:12:39 +0600

    complete chating part

--- src/main/chat/chat.service.ts
+++ src/main/chat/chat.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
