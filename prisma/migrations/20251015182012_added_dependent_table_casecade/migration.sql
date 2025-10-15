e6950e65c25a4374b4fdbfe7b01a2b0b9b3c204a
ranamiah221 <ranarasul21@gmail.com>
2025-10-16 00:23:28 +0600

    added define schema dependencies

---
+++ prisma/migrations/20251015182012_added_dependent_table_casecade/migration.sql
@@ -0,0 +1,35 @@
+-- DropForeignKey
+ALTER TABLE "public"."Message" DROP CONSTRAINT "Message_receiverId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Message" DROP CONSTRAINT "Message_senderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Payment" DROP CONSTRAINT "Payment_bookingId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Review" DROP CONSTRAINT "Review_spaceId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Review" DROP CONSTRAINT "Review_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Space" DROP CONSTRAINT "Space_ownerId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "Booking"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Space" ADD CONSTRAINT "Space_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
